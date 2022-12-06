54101a1d9eb1771314959118fbefe800ffe917f8
Xostron <aat9044107480@gmail.com>
2022-12-06 19:48:34 +0300

    hook useSchedular

---
+++ client/src/hooks/useInterval.tsx
@@ -0,0 +1,35 @@
+import { DataResult } from "@remix-run/router/dist/utils";
+import React, { FC, useState, useEffect, useRef } from "react";
+
+
+// при изменении delay
+
+export const useSchedular = (callback: () => void, delay?: number) => {
+
+    const updCallback = useRef<() => void>(callback)
+
+    const [currentTime, setCurrentTime] = useState<Date>(new Date())
+
+    // подписка на callback
+    useEffect(() => {
+        updCallback.current = callback
+    }, [callback])
+
+    // подписка на delay и монтирование-запускается таймер, 
+    // размонтирование - удаляется таймер
+    useEffect(() => {
+        function tick() {
+            updCallback.current();
+        }
+        if (delay !== null) {
+            let id = setInterval(tick, delay);
+            return () => clearInterval(id);
+        }
+    }, [delay]);
+
+    return { currentTime }
+}
+
+// заметка
+// Не удается вызвать объект, который может иметь значение "undefined".ts(2722)
+// updCallback.current?.()

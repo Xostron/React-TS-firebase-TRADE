1a4263d9a29f6e4dd3f8f8699d1aa2cb7e6f0149
Xostron <aat9044107480@gmail.com>
2022-12-14 18:30:38 +0300

    typescript компонет InputNumber

---
+++ client/src/components/UI/input/input-number/InputNumber.tsx
@@ -0,0 +1,43 @@
+import React, { FC } from "react";
+import style from './InputNumber.module.less'
+import { HandySvg } from "handy-svg";
+
+interface IInputNumber {
+    name: string,
+    placeholder: string,
+    value: number,
+    changeHandler: (e: React.ChangeEvent<HTMLInputElement>) => void,
+}
+
+interface IInputNumberProps {
+    props: IInputNumber
+}
+
+export const InputNumber: FC<IInputNumberProps> = ({ props }) => {
+
+    const {
+        name,
+        placeholder,
+        value,
+        changeHandler,
+    } = props
+
+    return (
+        <div className={style.container}>
+
+            {/* {value ?
+                <span className={style.name}>{placeholder}:</span>
+                :
+                <span className={style.name}>{placeholder}...</span>
+            } */}
+
+            <input
+                className={style.numb}
+                name={name}
+                value={value}
+                type="number"
+                onChange={(e) => changeHandler(e)}
+            />
+        </div>
+    )
+}
